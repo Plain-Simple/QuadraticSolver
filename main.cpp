be7bd117dbd06e8e6f40513b152641c7a68454ca
Matthew <unsolvedcypher@gmail.com>
2014-11-10 16:09:42 -0500

    code refactoring/optimization

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /// REMINDER: Function names should be command verbs (ie. PrintTable(), FindIntercepts())
 
+/// note - we need to figure out how to use the settings. Should all the functions be called and the functions decide whether to run, or should functions only be called if they are set to run in settins?
 
 
 /** COMMENTS ********************************************************/
@@ -19,26 +20,42 @@ using namespace std;
 
 
 /** FUNCTION PROTOTYPES**********************************************/
-/* equation_standard[3], vertex[2], solution[2], has_solution is a bad name */
-void InputEquation(double equation_standard[3], double vertex[2], double solution[2], bool & has_solution);
-void CalculateRoots(double equation_standard[3], double vertex[2], double solution[2], bool & has_solution);
-void CalculateVertex(double equation_standard[3], double vertex[2], double solution[2], bool & has_solution);
+/* equation[3], vertex[2], solution[2], has_solution is a bad name */
+/// please explain what makes them bad
+/* gets equation from the user */
+void InputEquation(double equation[3]);
+/* finds the quadratic rots of the equation */
+void CalculateRoots(double equation[3], double solution[2], bool & has_solution);
+/* finds the vertex of the parabola */
+void CalculateVertex(double equation[3], double vertex[2]);
+/// description needed, also options needs to be a verb
 void Options(bool & calc_vertex, bool & calc_yintercept, bool & print_table, bool & calc_sumproduct, bool & factor_equation, int table_boundaries[2]);
-void Intercepts(double equation_standard[3], double vertex[2], double solution[2], bool & has_solution);
-void Table(double equation_standard[3], double vertex[2], double solution[2], bool & has_solution, bool calc_vertex, bool calc_yintercept, bool print_table, bool calc_sumproduct, bool factor_equation, int table_boundaries[2]);
-void Display(double equation_standard[3], double vertex[2], double solution[2], bool & has_solution, bool calc_vertex, bool calc_yintercept, bool print_table, bool calc_sumproduct, bool factor_equation, int table_boundaries[2]);
-void Factor(double equation_standard[3], double vertex[2], double solution[2], bool & has_solution);
+/* takes in an equation and returns its y intercept */
+double CalculateYIntercept(double equation[3]);
+/* outputs a table of values for the equation */
+/// does it really need all these parameters?
+void Table(double equation[3], double vertex[2], double solution[2], bool & has_solution, bool calc_vertex, bool calc_yintercept, bool print_table, bool calc_sumproduct, bool factor_equation, int table_boundaries[2]);
+/// description needed
+void Display(double equation[3], double vertex[2], double solution[2], bool & has_solution, bool calc_vertex, bool calc_yintercept, bool print_table, bool calc_sumproduct, bool factor_equation, int table_boundaries[2]);
+/// description needed
+void Factor(double equation[3], double vertex[2], double solution[2], bool & has_solution);
+/// description needed - also, we need to decide on settings vs options
 void LoadSettings(bool & calc_vertex, bool & calc_yintercept, bool & print_table, bool & calc_sumproduct, bool & factor_equation, int table_boundaries[2]);
+/* returns the result of plugging int x into double equation[3] */
+double PlugIn(double x, double equation[3]);
+
+/* the following are type conversion functions: */
 string BoolToString(bool bool_to_convert);
 bool CharToBool(char c);
 char BoolToChar(bool b);
-string IntToString(int n);
+
 
 /** MAIN FUNCTION ***************************************************/
 int main() {
-  bool calc_vertex = true, calc_yintercept = true, print_table = true, calc_sumproduct = true, factor_equation = true;
+  bool calc_vertex = true, calc_yintercept = true, print_table = true,
+       calc_sumproduct = true, factor_equation = true;
   int table_boundaries[2] = { -10, 10};
-  double equation_standard[3], vertex[2], solution[2];
+  double equation[3], vertex[2], solution[2];
   bool has_solution;
   cout << "Plain+Simple Quadratic Solver\n";
   cout << "        Press enter\n";
@@ -54,18 +71,27 @@ int main() {
     cout << "---------------------------------\n";
     cout << "Enter choice: ";
     cin >> menu_input;
-    if(menu_input == 1) {
-      Options(calc_vertex, calc_yintercept, print_table, calc_sumproduct,
-              factor_equation, table_boundaries);
-    } else if(menu_input == 2) {
-      InputEquation(equation_standard, vertex, solution, has_solution);
-      CalculateRoots(equation_standard, vertex, solution, has_solution);
-      Display(equation_standard, vertex, solution, has_solution, calc_vertex,
-              calc_yintercept, print_table, calc_sumproduct, factor_equation,
-              table_boundaries);
-      /// TODO: Input checker, quit function
+    switch (menu_input) {
+      case 1:
+        Options(calc_vertex, calc_yintercept, print_table, calc_sumproduct,
+                factor_equation, table_boundaries);
+        break;
+      case 2:
+        InputEquation(equation);
+        CalculateRoots(equation, solution, has_solution);
+        Display(equation, vertex, solution, has_solution, calc_vertex,
+                calc_yintercept, print_table, calc_sumproduct, factor_equation,
+                table_boundaries);
+        break;
+      case 3:
+        exit(0);
+        break;
+      default:
+        /// check input
+        break;
     }
     cout << "\n\nHit enter to return to menu "; // not working
+     /// what isn't working? In what way?
     /// this happens when we use a combination of cin, getline, and other stuff. We will have to figure out one to use to standardize everything
     cin.get();
     cin.get(); /* has to be repeated once */
@@ -74,93 +100,77 @@ int main() {
 }
 
 /** FUNCTIONS *******************************************************/
-void InputEquation(double equation_standard[3], double vertex[2],
-                   double solution[2], bool & has_solution) {
+void InputEquation(double equation[3]) {
   cout << "Equation will be entered in the form ax^2 + bx + c = 0\n\n";
-  cout << "Enter << a >> term: ";
-  cin >> equation_standard[0];
-  cout << "Enter << b >> term: ";
-  cin >> equation_standard[1];
-  cout << "Enter << c >> term: ";
-  cin >> equation_standard[2];
+  for (int i = 0; i < 3; i++) {
+    cout << "Enter << " << i + 99 << " >> term: ";
+    cin >> equation[i];
+  }
 }
-void CalculateRoots(double equation_standard[3], double vertex[2], double solution[2],
-           bool & has_solution) {
-  int discriminant;
-  discriminant = equation_standard[1] * equation_standard[1] - 4 *
-                 equation_standard[0] *
-                 equation_standard[2];
-  if(discriminant < 0) {
+void CalculateRoots(double equation[3], double solution[2], bool & has_solution) {
+  /* b^2 - 4ac */
+  int discriminant = pow(equation[1], 2) - (4 * equation[0] * equation[2]);
+  if (discriminant < 0) {
     has_solution = false;
   } else {
     has_solution = true;
-    solution[0] = ((-1 * equation_standard[1]) + sqrt(equation_standard[1] *      equation_standard[1] - 4 *
-                   equation_standard[0] * equation_standard[2])) / (2 * equation_standard[0]);
-    solution[1] = ((-1 * equation_standard[1]) - sqrt(equation_standard[1] *
-                   equation_standard[1] - 4 *
-                   equation_standard[0] * equation_standard[2])) / (2 * equation_standard[0]);
+    /* -b + root b^2 - 4ac / 2a */
+    solution[0] = (-equation[1] + sqrt(discriminant)) / (2 * equation[0]);
+    /* -b - root b^2 - 4ac / 2a */
+    solution[1] = (-equation[1] - sqrt(discriminant)) / (2 * equation[0]);
   }
 }
-void CalculateVertex(double equation_standard[3], double vertex[2], double solution[2],
-            bool & has_solution) {
-  vertex[0] = (equation_standard[1] * -1) / (2 * equation_standard[0]);
-  vertex[1] = equation_standard[0] * vertex[0] * vertex[0] + equation_standard[1]
-              *
-              vertex[0] + equation_standard[2];
+void CalculateVertex(double equation[3], double vertex[2]) {
+  vertex[0] = (-equation[1]) / (2 * equation[0]);
+  vertex[1] = PlugIn(vertex[0], equation);
 }
-void Intercepts(double equation_standard[3], double vertex[2],
-                double solution[2], bool & has_solution) {
-  //		y_intercept_value = equation_standard[2]; /// can't remember the purpose of this function
+double CalculateYIntercept(double equation[3]) {
+  return PlugIn(0, equation);
 }
 void Options(bool & calc_vertex, bool & calc_yintercept, bool & print_table,
              bool & calc_sumproduct, bool & factor_equation, int table_boundaries[2]) {
-	string string_settings = "";
   LoadSettings(calc_vertex, calc_yintercept, print_table, calc_sumproduct,
                factor_equation, table_boundaries);
-  cout << "\n\nSettings: \n";
-  cout << "---------------------------------------------\n";
-  string symbol = BoolToString(calc_vertex);
-  cout << "1. Calculate Vertex......................." << symbol << endl;
-  symbol = BoolToString(calc_yintercept);
-  cout << "2. Calculate y-Intercept.................." << symbol << endl;
-  symbol = BoolToString(calc_sumproduct);
-  cout << "3. Calculate Sum and Product.............." << symbol << endl;
-  symbol = BoolToString(factor_equation);
-  cout << "4. Factor Equation........................" << symbol << endl;
-  symbol = BoolToString(print_table);
-  cout << "5. Print Table............................" << symbol << endl;
-  cout << "6. Set Table Boundaries...................("
-       << table_boundaries[0] << "," << table_boundaries[1] << ")\n"; /// looks like we have some problems here, I'll fix that very soon
-  cout << "7. Back to Main Menu\n";
-  cout << "---------------------------------------------\n\n";
-  cout << "Enter number of choice you would like to change: ";
+  cout << "\n\nSettings: \n"
+       << "---------------------------------------------\n"
+       << "1. Calculate Vertex......................."
+       << BoolToString(calc_vertex)
+       << "\n2. Calculate y-Intercept.................."
+       << BoolToString(calc_yintercept)
+       << "\n3. Calculate Sum and Product.............."
+       << BoolToString(calc_sumproduct)
+       << "\n4. Factor Equation........................"
+       << BoolToString(factor_equation)
+       << "\n5. Print Table............................"
+       << BoolToString(print_table)
+       << "\n6. Set Table Boundaries...................("
+       /// looks like we have some problems here, I'll fix that very soon
+       << table_boundaries[0] << "," << table_boundaries[1] << ")\n"
+       << "\n7. Back to Main Menu\n"
+       << "---------------------------------------------\n\n"
+       << "Enter number of choice you would like to change: ";
   int choice;
   cin >> choice;
   switch(choice) {
   case 1:
     calc_vertex = !calc_vertex;
-    symbol = BoolToString(calc_vertex);
-    cout << "Calculate Vertex set to << " << symbol << " >>\n";
+    cout << "Calculate Vertex set to << " << BoolToString(calc_vertex) << " >>\n";
     break;
   case 2:
     calc_yintercept = ! calc_yintercept;
-    symbol = BoolToString(calc_yintercept);
-    cout << "Calculate y-Intercept set to << " << symbol << " >>\n";
+    cout << "Calculate y-Intercept set to << " << BoolToString(calc_yintercept) << " >>\n";
     break;
   case 3:
     print_table = !print_table;
-    symbol = BoolToString(print_table);
-    cout << "Print Table set to << " << symbol << " >>\n";
+    cout << "Print Table set to << " << BoolToString(print_table) << " >>\n";
     break;
   case 4:
     calc_sumproduct = !calc_sumproduct;
-    symbol = BoolToString(calc_sumproduct);
-    cout << "Calculate Sum and Product set to << " << symbol << " >>\n";
+    cout << "Calculate Sum and Product set to << " << BoolToString(calc_sumproduct) << " >>\n";
     break;
   case 5:
     factor_equation = !factor_equation;
-    symbol = BoolToString(factor_equation);
-    cout << "Factor Equation set to << " << symbol << " >>\n";
+    cout << "Factor Equation set to << " << BoolToString(factor_equation) << " >>\n";
     break;
   case 6:
     cout << "Enter lower bound: ";
@@ -175,9 +185,7 @@ void Options(bool & calc_vertex, bool & calc_yintercept, bool & print_table,
     /// it should give the user an error
     break;
   }
-  string_settings = string_settings + BoolToChar(calc_vertex) + BoolToChar(calc_yintercept) + BoolToChar(print_table);
-  string_settings = string_settings + BoolToChar(calc_sumproduct) + BoolToChar(factor_equation);
-  string_settings = string_settings + IntToString(table_boundaries[0]) + ',' + IntToString(table_boundaries[1]);
+  string string_settings = "" + BoolToChar(calc_vertex) + BoolToChar(calc_yintercept) + BoolToChar(print_table) + BoolToChar(calc_sumproduct) + BoolToChar(factor_equation) + to_string(table_boundaries[0]) + ',' + to_string(table_boundaries[1]);
 
   ofstream settings_file("quadratics_settings");
   if(settings_file.is_open()) {
@@ -185,45 +193,43 @@ void Options(bool & calc_vertex, bool & calc_yintercept, bool & print_table,
   }
   settings_file.close();
 }
-void Table(double equation_standard[3], double vertex[2], double solution[2],
-           bool & has_solution, bool calc_vertex, bool calc_yintercept, bool print_table,
-           bool calc_sumproduct, bool factor_equation, int table_boundaries[2]) {
+void Table(double equation[3], bool print_table, int table_boundaries[2]) {
   cout << "   x             f(x)\n";
   for(int i = table_boundaries[0]; i <= table_boundaries[1]; i++) {
-    cout << "   " << i << "          " << equation_standard[0] * i * i +
-         equation_standard[1] * i
-         + equation_standard[2] << endl;
+    cout << "   " << i << "          " << PlugIn(i, equation) << endl;
   }
 }
-void Display(double equation_standard[3], double vertex[2], double solution[2],
+void Display(double equation[3], double vertex[2], double solution[2],
              bool & has_solution, bool calc_vertex, bool calc_yintercept, bool print_table,
              bool calc_sumproduct, bool factor_equation, int table_boundaries[2]) {
 	cout << endl << endl;
-  if(calc_vertex == 1) {
-    CalculateVertex(equation_standard, vertex, solution, has_solution);
+  if(calc_vertex) {
+    CalculateVertex(equation, vertex);
   }
-  if(calc_yintercept == 1) {
-    Intercepts(equation_standard, vertex, solution, has_solution);
+  if(calc_yintercept) {
+    /// ok, it's calculated - now it needs to be stored!
+    CalculateYIntercept(equation);
   }
+  /// please comment the following lines
   cout << "Equation: ";
-  if(equation_standard[0] == 1)
+  if(equation[0] == 1)
 	  cout << "x^2 ";
-  else if(equation_standard[0] != 0)
-	  cout << equation_standard[0] << "x^2 ";
-  if(equation_standard[1] > 0)
+  else if(equation[0] != 0)
+	  cout << equation[0] << "x^2 ";
+  if(equation[1] > 0)
 	  cout << "+ ";
-  else if(equation_standard[0] < 0)
+  else if(equation[0] < 0)
 	  cout << "- ";
-  if(equation_standard[1] == 1)
+  if(equation[1] == 1)
 	  cout << "x ";
-  else if(equation_standard[1] != 0)
-	  cout << equation_standard[1] << "x ";
-  if(equation_standard[2] > 0)
+  else if(equation[1] != 0)
+	  cout << equation[1] << "x ";
+  if(equation[2] > 0)
 	  cout << "+ ";
-  else if(equation_standard[2] < 0)
+  else if(equation[2] < 0)
 	  cout << "- ";
-  if(equation_standard[2] != 0)
-       cout << equation_standard[2];
+  if(equation[2] != 0)
+       cout << equation[2];
   cout << " = 0\n";
   if(!has_solution) {
     cout << "Equation has no solutions\n";
@@ -241,39 +247,38 @@ void Display(double equation_standard[3], double vertex[2], double solution[2],
     cout << "Product of roots = " << solution[0] * solution[1] << endl;
   }
   if(factor_equation) {
-    Factor(equation_standard, vertex, solution, has_solution);
+    Factor(equation, vertex, solution, has_solution);
   }
   if(calc_vertex == 1) {
     cout << "Vertex: (" << vertex[0] << "," << vertex[1] << ")\n";
   }
   if(calc_yintercept == 1) {
-    cout << "y-Intercept: (0," << equation_standard[1] << ")\n";
+    cout << "y-Intercept: (0," << equation[1] << ")\n";
   }
   if(print_table == 1) {
-    Table(equation_standard, vertex, solution, has_solution, calc_vertex,
-          calc_yintercept, print_table, calc_sumproduct, factor_equation,
-          table_boundaries);
+    Table(equation, print_table, table_boundaries);
   }
 }
-void Factor(double equation_standard[3], double vertex[2], double solution[2],
+void Factor(double equation[3], double vertex[2], double solution[2],
             bool & has_solution) {
 	cout << "Factored: ";
-  if(solution[0] == solution[1]) {
-    if(equation_standard[0] == 0)
+  if(solution[0] == solution[1] && has_solution) {
+    if(equation[0] == 0) /* should this even happen? That's not a quadratic. */
     	cout << "0";
-    else if(equation_standard[0] != 1)
-	  cout << equation_standard[0];
-	if(equation_standard[0] != 0) {
+    else if(equation[0] != 1) {
+      cout << equation[0];
+    }
+	if(equation[0] != 0) {
 	  cout << "(x ";
 	  if(solution[0] == 0)
 		  cout << ")^2\n";
 	  else if(solution[0] > 1)
 		  cout << "- " << solution[0] << ")^2\n";
 	  else
-		  cout << "+ " << -1 * solution[0] << ")^2\n";
+		  cout << "+ " << -solution[0] << ")^2\n";
     }
   } else {
-    cout << equation_standard[0] << "(x-" << solution[0] << ")(x-" << solution[1] <<
+    cout << equation[0] << "(x-" << solution[0] << ")(x-" << solution[1] <<
          ")\n";
   }
 }
@@ -299,7 +304,7 @@ void LoadSettings(bool & calc_vertex, bool & calc_yintercept,
 			print_table = CharToBool(line[2]);
 			calc_sumproduct = CharToBool(line[3]);
 			factor_equation = CharToBool(line[4]);
-			for(int i = 5; i < line.size(); i++) {
+			for(int i = 5; i < (int)line.size(); i++) {
 				if(line[i] == ',') {
 					comma_reached = 1;
 				}
@@ -338,24 +343,8 @@ char BoolToChar(bool b) {
 	else
 		return '1';
 }
-string IntToString(int n) { /// had to write my own function because there weren't any good references online
-	bool is_negative = 0;
-	char digit;
-	int number, remainder; /// kind of bad names
-	string s = "";
-	number = n;
-	if(number < 0) {
-		is_negative = 1;
-		number = number * -1;
-	} while(number != 0) { /* loops until whole number has been converted */
-		remainder = number % 10;
-		number = number / 10;
-		digit = char(remainder + 48); /* convert remainder to a char */
-		s = digit + s; /* build s */
-	} if(is_negative)
-		s = '-' + s;
-	return s;
+
+double PlugIn(double x, double equation[3]) {
+  /* ax^2 + bx + c */
+  return (equation[0] * pow(x, 2)) + (equation[1] * x) + (equation[2]);
 }
-
-
-
